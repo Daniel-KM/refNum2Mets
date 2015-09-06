3873df91cba342c5d1e5bd4dcf8e22884f5a8c5a
Daniel Berthereau <Daniel.github@Berthereau.net>
2015-09-07 00:00:00 +0200

    Ajout d'un script pour traiter des dossiers.

---
+++ refNum2Mets.sh
@@ -0,0 +1,107 @@
+#!/bin/sh
+#
+# Convertit tous les fichiers refNum d'un dossier en Mets via xslt Saxon-HE.
+#
+# - Les fichiers originaux sont conservés.
+# - Le hash de tous les fichiers est calculé, ce qui peut être long.
+# - Les noms de fichiers ne doivent pas contenir d'espace (problème dans
+# l'extraction des tailles de fichiers).
+#
+# Utilise :
+# - xmllint
+# - saxon-he
+#
+# Auteur Daniel Berthereau <daniel.berthereau@mines-paristech.fr>
+# Copyright Daniel Berthereau, 2015-09-07
+# Licence CeCILL v2.1
+#
+# Commande :
+# refNum2Mets.sh dossier
+
+dossier=$1
+
+preparehash='true'
+reindente='true'
+
+# Valeurs par défaut.
+if [ "$dossier" = '' ]
+then
+    dossier=$(pwd)
+fi
+
+filehashs='filehashs.txt'
+filesizes='filesizes.txt'
+arks='arks.txt'
+
+# Directory du script
+# Absolute path to this script, e.g. /home/user/bin/foo.sh
+SCRIPT=$(readlink -f "$0")
+# Absolute path this script is in, thus /home/user/bin
+SCRIPTPATH=$(dirname "$SCRIPT")
+
+xslpath="$SCRIPTPATH/refNum2Mets.xsl"
+xslidentitypath="$SCRIPTPATH/identity.xsl"
+
+echo Traitement du dossier \"$dossier\" via \"$xslpath\".
+echo
+
+# Traitement du dossier
+file -i $dossier/* | grep --ignore-case 'application/xml' | awk -F':' '{ print $1 }' | while read file
+do
+    # Vérification rapide si c'est un fichier refNum (sans validation avancée).
+    xmlroot=$(xmllint --xpath 'local-name(/*)' "$file")
+    if [ "$xmlroot" != 'refNum' ]
+    then
+        continue
+    fi
+
+    dirname=$(dirname "$file")
+    filename=$(basename "$file")
+    name="${filename%.*}"
+    extension="${filename##*.}"
+
+    # Vérifie s'il y a une double extension (.refnum.xml).
+    if [ "${name##*.}" = 'refnum' ]
+    then
+        name="${name%.*}"
+    fi
+
+    # Prépare le nom du fichier mets.
+    metsfilename="$name.mets.xml"
+    metspath="$dirname/$metsfilename"
+
+    # Compte le nombre de fichiers pour info.
+    nombrefichiers=$(find $dirname -follow -type f | wc -l)
+    taillefichiers=$(du -sh | cut -f 1)
+
+    echo $filename '=>' $metsfilename \($nombrefichiers fichiers, $taillefichiers\)
+
+    # Préparation des taille des fichiers du dossier courant.
+    taillepath="$dirname/$filesizes"
+    find . -type f -print0 | xargs -0 stat --format '%n %s' | cut -sd / -f 2- | sort | awk '{print $2"  "$1}' > "$taillepath"
+
+    # Préparation des hashs des fichiers du dossier courant.
+    if [ "$preparehash" = 'true' ]
+    then
+        echo '  ' Calcul des hash sha1 des fichiers du dossier...
+
+        hashpath="$dirname/$filehashs"
+        find . -type f | cut -sd / -f 2- | sort | xargs sha1sum > "$hashpath"
+    fi
+
+    # Commande pour Debian 8.
+    CLASSPATH=/usr/share/java/Saxon-HE.jar java net.sf.saxon.Transform -ext:on -versionmsg:off -s:"$file" -xsl:"$xslpath" -o:"$metspath"
+
+    # Améliore l'indentation, ce qui est impossible avec la version libre de
+    # saxon.
+    if [ "$reindente" = 'true' ]
+    then
+        xmllint --format --recover --output "$metspath" "$metspath"
+    fi
+
+done
+
+echo
+echo Traitement terminé.
+
+exit 0
